3c33592cd7948a49d1fe0b493e48a25a4e1e894c
Giuliani D. Sanches <giulianisanches@userss.noreply.github.com>
2023-10-16 20:04:18 -0300

    feat: should deal with objects

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,7 +1,10 @@
 var assert = require("assert");
 
 describe("Basic mocha test", function () {
-  it("Should throw errors", function () {
-    //assert.equal(2, 3);
+  it("Should deal with objects", function () {
+    let obj = { name: "Jon", gender: "male" };
+    let objB = { name: "Jon", gender: "male" };
+
+    obj.should.deep.equal(objB);
   });
 });
